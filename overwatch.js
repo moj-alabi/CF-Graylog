a354d738de3f16150518885bd920a437511e4f3b
MOJ <43385016+moj-alabi@users.noreply.github.com>
2025-09-14 22:36:48 +0100

    Create overwatch.js

---
+++ overwatch.js
@@ -0,0 +1,29 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const log = {
+    version: "1.1",
+    host: request.headers.get("host"),
+    short_message: "Cloudflare Request",
+    fields: {
+      ip: request.headers.get("cf-connecting-ip"),
+      ua: request.headers.get("user-agent"),
+      path: new URL(request.url).pathname,
+      bot_score: request.headers.get("cf-bot-score") || 0,
+      threat_score: request.headers.get("cf-threat-score") || 0,
+      country: request.headers.get("cf-ipcountry")
+    }
+  }
+
+  // send to Graylog
+  await fetch("https://graylog.example.com:12201/gelf", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(log)
+  })
+
+  // pass request to your origin
+  return fetch(request)
+}
